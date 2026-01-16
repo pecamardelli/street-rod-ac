c595bd57c54b5d009fc4bf4933c12795fabdae4d
Pablo Camardelli <pecamardelli@gmail.com>
2026-01-16 00:59:41 -0300

    Reuse bottom bar component

---
+++ Street Rod AC/Components/BottomBar/BottomBarView.xaml.cs
@@ -0,0 +1,32 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Street_Rod_AC.Components.BottomBar
+{
+    /// <summary>
+    /// Reusable bottom bar component that displays bankroll information
+    /// </summary>
+    public partial class BottomBarView : UserControl
+    {
+        public static readonly DependencyProperty BankrollDisplayProperty =
+            DependencyProperty.Register(
+                nameof(BankrollDisplay),
+                typeof(string),
+                typeof(BottomBarView),
+                new PropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// The formatted bankroll text to display (e.g., "$1,000")
+        /// </summary>
+        public string BankrollDisplay
+        {
+            get => (string)GetValue(BankrollDisplayProperty);
+            set => SetValue(BankrollDisplayProperty, value);
+        }
+
+        public BottomBarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
